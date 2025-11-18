a1dd19512ab5f9e35e12d9341fad0a4ea14aa2a6
Jay <59494201+JMathieu007@users.noreply.github.com>
2025-11-18 13:17:45 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,17 @@
 terraform {
   required_providers {
-    azurerm = { source = "hashicorp/azurerm", version = "~>3.0" }
-    random  = { source = "hashicorp/random", version = "~>3.6" }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.6"
+    }
   }
+
   required_version = ">=1.4"
- 
+
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
     storage_account_name = "tfstatelola2025"
@@ -12,9 +19,11 @@ terraform {
     key                  = "lesson6.terraform.tfstate"
   }
 }
- 
-provider "azurerm" { features {} }
- 
+
+provider "azurerm" {
+  features {}
+}
+
 resource "random_string" "suffix" {
   length  = 6
   upper   = false
@@ -22,18 +31,18 @@ resource "random_string" "suffix" {
   numeric = true
   special = false
 }
- 
+
 locals {
   location      = "East US"
   app_base_name = "lesson6-html-app"
   app_name      = "${local.app_base_name}-${random_string.suffix.result}"
 }
- 
+
 resource "azurerm_resource_group" "rg" {
   name     = "lesson6-jmm"
   location = local.location
 }
- 
+
 resource "azurerm_service_plan" "plan" {
   name                = "lesson6-service-plan"
   location            = azurerm_resource_group.rg.location
@@ -41,13 +50,13 @@ resource "azurerm_service_plan" "plan" {
   os_type             = "Linux"
   sku_name            = "B1"
 }
- 
+
 resource "azurerm_linux_web_app" "webapp" {
   name                = local.app_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.plan.id
- 
+
   site_config {
     app_command_line = ""
   }
